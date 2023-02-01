38ca13cd9b6aa452ef7102d4653b0c1e86ecbe84
FrankAsplund <frank_asplund@hotmail.com>
2023-02-01 11:41:40 +0100

    Init commit

---
+++ vuetify-project/src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home-container">
+    <h1>Jordbruksverket prototyp form</h1>
+
+    <div>
+      <Navbar />
+    </div>
+    <div>
+      <Form />
+    </div>
+
+  </div>
+</template>
+
+<script setup>
+  import Navbar from "../components/Navbar.vue"
+  import Form from "../components/Form.vue"
+</script>
+
+<style>
+.h1 {
+  display: flex;
+  justify-content: center;
+}
+</style>
