12d0fa155cbd454cb65a8dae87504209ac2debec
FrankAsplund <frank_asplund@hotmail.com>
2023-02-06 13:36:43 +0100

    Moved navbar into Home

--- vuetify-project/src/views/Home.vue
+++ vuetify-project/src/views/Home.vue
@@ -1,19 +1,48 @@
 <template>
   <div class="home-container">
-    <h1>Jordbruksverket prototyp form</h1>
+    <v-card>
+      <v-tabs
+        v-model="tab"
+        color="primary"
+      >
+        <v-tab value="one">Grunduppgifter</v-tab>
+        <v-tab value="two">Ansökan</v-tab>
+        <v-tab value="three">Summering</v-tab>
+        <v-tab value="four">Klar</v-tab>
+      </v-tabs>
+  
+      <v-card-text>
+        <v-window v-model="tab">
+          <v-window-item value="one">
+            Grunduppgifter 
+          </v-window-item>
+  
+          <v-window-item value="two">
+            Ansökan
+          </v-window-item>
+  
+          <v-window-item value="three">
+            Summering
+          </v-window-item>
 
-    <div>
-      <Navbar />
+          <v-window-item value="four">
+            Klar
+          </v-window-item>
+        </v-window>
+      </v-card-text>
+    </v-card>
     </div>
-    <div>
-      <Form />
-    </div>
+  </template>
 
-  </div>
-</template>
+<script>
+export default {
+  data: () => ({
+    tab: null,
+  })
+}
+</script>
 
 <script setup>
-  import Navbar from "../components/Navbar.vue"
   import Form from "../components/Form.vue"
 </script>
 
