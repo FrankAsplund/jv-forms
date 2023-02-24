441813958c655518912d4d4affbf0b8df6aa1ec9
FrankAsplund <frank_asplund@hotmail.com>
2023-02-24 15:14:01 +0100

    FormApply progress (WIP)

--- vuetify-project/src/components/Form.vue
+++ vuetify-project/src/components/Form.vue
@@ -4,50 +4,101 @@
       <c-body body="Ange dina uppgifter" />
       <v-row variant="outlined">
         <v-row variant="outlined" class="ma-2 pa-0">
-          <v-text-field label="Förnamn" type="text" model-value="John" prepend-inner-icon="mdi-account-box"
-            disabled></v-text-field>
-          <v-text-field label="Efternamn" model-value="Doe" type="text" prepend-inner-icon="mdi-account-box"
-            disabled></v-text-field>
-          <v-text-field label="Personnummer" type="text" model-value="12456-1234" prepend-inner-icon="mdi-account-box"
-            disabled></v-text-field>
+          <v-text-field
+            label="Förnamn"
+            type="text"
+            model-value="John"
+            prepend-inner-icon="mdi-account-box"
+            disabled
+          ></v-text-field>
+          <v-text-field
+            label="Efternamn"
+            model-value="Doe"
+            type="text"
+            prepend-inner-icon="mdi-account-box"
+            disabled
+          ></v-text-field>
+          <v-text-field
+            label="Personnummer"
+            type="text"
+            model-value="12456-1234"
+            prepend-inner-icon="mdi-account-box"
+            disabled
+          ></v-text-field>
         </v-row>
         <v-col cols="12" md="12">
-          <v-text-field label="E-post" type="email" prepend-inner-icon="mdi-email" v-model="formData.email"
-            hint="Skriv in din e-postadress."></v-text-field>
-          <v-text-field label="Upprepa E-post" type="email" prepend-inner-icon="mdi-email"
+          <v-text-field
+            label="E-post"
+            type="email"
+            prepend-inner-icon="mdi-email"
+            v-model="formData.email"
+            hint="Skriv in din e-postadress."
+          ></v-text-field>
+          <v-text-field
+            label="Upprepa E-post"
+            type="email"
+            prepend-inner-icon="mdi-email"
             hint="Skriv in samma e-postadress som ovan."
-            ></v-text-field>
+          ></v-text-field>
 
-          <v-text-field label="Telefonnummer" type="text" v-model="formData.phoneNumber" prepend-inner-icon="mdi-phone"
-            :rules="[
-              v => !!v || 'Field is required'
-            ]"></v-text-field>
+          <v-text-field
+            label="Telefonnummer"
+            type="text"
+            v-model="formData.phoneNumber"
+            prepend-inner-icon="mdi-phone"
+            :rules="[(v) => !!v || 'Field is required']"
+          ></v-text-field>
         </v-col>
       </v-row>
 
       <v-row variant="tonal" class="pa-2">
         <v-col>
-          <c-body body="Söker du som privatperson/enskild firma eller som ombud?" />
-          <v-select prepend-inner-icon="mdi-form-select" placeholder="Jag söker som..." :items="applyChoice"
-            v-model="formData.apply" :rules="[
-        v => !!v || 'Field is required'
-      ]"></v-select>
+          <c-body
+            body="Söker du som privatperson/enskild firma eller som ombud?"
+          />
+          <v-select
+            prepend-inner-icon="mdi-form-select"
+            placeholder="Jag söker som..."
+            :items="applyChoice"
+            v-model="formData.apply"
+            :rules="[(v) => !!v || 'Field is required']"
+          ></v-select>
         </v-col>
       </v-row>
-      <v-container v-if="formData.apply === 'Jag söker som privatperson/enskild firma'">
+      <v-container
+        v-if="formData.apply === 'Jag söker som privatperson/enskild firma'"
+      >
         <v-container class="my-2">
           <c-body body="Kundnummer" />
-          <v-select label="Länsbokstav" item-title="name" :items="items" prepend-inner-icon="mdi-format-letter-case"
-            return-object v-model="formData.applyCounty"></v-select>
-          <v-text-field type="text" label="Nummer" placeholder="XXXXXX" prepend-inner-icon="mdi-pound-box"
+          <v-select
+            label="Länsbokstav"
+            item-title="name"
+            :items="items"
+            prepend-inner-icon="mdi-format-letter-case"
+            return-object
+            v-model="formData.applyCounty"
+          ></v-select>
+          <v-text-field
+            type="text"
+            label="Nummer"
+            placeholder="XXXXXX"
+            prepend-inner-icon="mdi-pound-box"
             hint="Ange de upp till 6 siffror som tillsammans med länsbokstaven utgör stödmottagarens kundnummer. Observera att kundnummer INTE är samma som produktionsplatsnummer (SE-nr)."
-            v-model="formData.applyNumber"></v-text-field>
+            v-model="formData.applyNumber"
+          ></v-text-field>
         </v-container>
       </v-container>
     </v-container>
 
     <v-container class="d-flex justify-center mx-2">
-      <v-btn type="submit" size="large" color="success" width="200" variant="tonal">Submit</v-btn>
+      <v-btn
+        type="submit"
+        size="large"
+        color="success"
+        width="200"
+        variant="tonal"
+        >Submit</v-btn
+      >
     </v-container>
   </v-form>
 </template>
@@ -78,7 +129,7 @@ export default {
         "Jag söker som ombud för privatperson/enskild firma",
         "Jag söker som ombud för ett företag eller organisation",
       ],
-     /*  rules: [
+      /*  rules: [
         value => {
           if (value) return true
           return 'You must enter an input.'
--- vuetify-project/src/components/FormApply.vue
+++ vuetify-project/src/components/FormApply.vue
@@ -1,136 +1,198 @@
 <template>
-    <v-container>
-        <v-form>
-            <c-body body="Produktion" />
-            <div>
-                <v-hover v-slot="{ isHovering, props }">
-                    <v-card class="w-75 h-75 my-4" v-bind="props" :color="isHovering ? 'light-green-lighten-5' : undefined">
-                        <v-checkbox v-model="productionType" label="Jag söker krisstöd för min fjäderfäproduktion"
-                            color="success" hide-details />
-                    </v-card>
-                </v-hover>
-            </div>
+  <v-container>
+    <v-form>
+      <c-body body="Produktion" />
+      <div>
+        <v-hover v-slot="{ isHovering, props }">
+          <v-card
+            class="w-75 h-75 my-4"
+            v-bind="props"
+            :color="isHovering ? 'light-green-lighten-5' : undefined"
+          >
+            <v-checkbox
+              v-model:if="productionType"
+              v-model:form="formDataApply.productionType"
+              label="Jag söker krisstöd för min fjäderfäproduktion"
+              color="success"
+              hide-details
+            />
+          </v-card>
+        </v-hover>
+      </div>
 
-            <v-container v-if="productionType">
-                <v-container>
-                    <v-checkbox label="Hönsproduktion" color="success" v-model="showHens" hide-details />
-                    <v-card class="w-100 h-75 my-4" v-if="showHens">
-                        <v-text-field v-model="field1" type="text"
-                            label="Ange genomsnittlig antal höns äldre än 20 veckor i din besättning 2023"
-                            prepend-icon="mdi-plus-box"></v-text-field>
+      <v-container v-if="productionType">
+        <v-container>
+          <v-checkbox
+            label="Hönsproduktion"
+            color="success"
+            v-model="showHens"
+            hide-details
+          />
+          <v-card class="w-100 h-75 my-4" v-if="showHens">
+            <v-text-field
+              v-model="field1"
+              type="text"
+              label="Ange genomsnittlig antal höns äldre än 20 veckor i din besättning 2023"
+              prepend-icon="mdi-plus-box"
+            ></v-text-field>
 
-                        <v-text-field v-model="field2" type="text"
-                            label="Ange antal slaktkycklingar i en normal uppfödningsomgång under år 2023"
-                            prepend-icon="mdi-plus-box"></v-text-field>
+            <v-text-field
+              v-model="field2"
+              type="text"
+              label="Ange antal slaktkycklingar i en normal uppfödningsomgång under år 2023"
+              prepend-icon="mdi-plus-box"
+            ></v-text-field>
 
-                        <v-text-field v-model="field3" type="text"
-                            label="Ange genomsnittligt antal kycklingar för äggproduktion i din besättning 2023"
-                            prepend-icon="mdi-plus-box"></v-text-field>
+            <v-text-field
+              v-model="field3"
+              type="text"
+              label="Ange genomsnittligt antal kycklingar för äggproduktion i din besättning 2023"
+              prepend-icon="mdi-plus-box"
+            ></v-text-field>
 
-                        <v-text-field label="Total summa" v-model="totalHens" readonly
-                            prepend-icon="mdi-equal-box"></v-text-field>
-                    </v-card>
-                </v-container>
-            </v-container>
+            <v-text-field
+              label="Total summa"
+              v-model="totalHens"
+              readonly
+              prepend-icon="mdi-equal-box"
+            ></v-text-field>
+          </v-card>
+        </v-container>
+      </v-container>
 
-            <v-container v-if="productionType">
-                <v-container>
-                    <v-checkbox label="Kalkonproduktion" color="success" v-model="showTurkeys" hide-details />
-                    <v-card class="w-100 h-75 my-4" v-if="showTurkeys">
-                        <v-text-field v-model="field4"
-                            label="Ange genomsnittligt antal kalkoner över 24 veckor i din besättning 2023"
-                            prepend-icon="mdi-plus-box"></v-text-field>
+      <v-container v-if="productionType">
+        <v-container>
+          <v-checkbox
+            label="Kalkonproduktion"
+            color="success"
+            v-model="showTurkeys"
+            hide-details
+          />
+          <v-card class="w-100 h-75 my-4" v-if="showTurkeys">
+            <v-text-field
+              v-model="field4"
+              label="Ange genomsnittligt antal kalkoner över 24 veckor i din besättning 2023"
+              prepend-icon="mdi-plus-box"
+            ></v-text-field>
 
-                        <v-text-field v-model="field5"
-                            label="Ange antal slaktkalkoner i en normal uppfödningsomgång under år 2023"
-                            prepend-icon="mdi-plus-box"></v-text-field>
+            <v-text-field
+              v-model="field5"
+              label="Ange antal slaktkalkoner i en normal uppfödningsomgång under år 2023"
+              prepend-icon="mdi-plus-box"
+            ></v-text-field>
 
-                        <v-text-field v-model="field6"
-                            label="Ange genomsnittligt antal kalkonkycklingar för äggproduktion i din besättning 2023"
-                            prepend-icon="mdi-plus-box"></v-text-field>
+            <v-text-field
+              v-model="field6"
+              label="Ange genomsnittligt antal kalkonkycklingar för äggproduktion i din besättning 2023"
+              prepend-icon="mdi-plus-box"
+            ></v-text-field>
 
-                        <v-text-field label="Total summa" v-model="totalTurkeys" readonly prepend-icon="mdi-equal-box">
-                        </v-text-field>
-                    </v-card>
-                </v-container>
-            </v-container>
+            <v-text-field
+              label="Total summa"
+              v-model="totalTurkeys"
+              readonly
+              prepend-icon="mdi-equal-box"
+            >
+            </v-text-field>
+          </v-card>
+        </v-container>
+      </v-container>
 
-            <v-container class="productionplace-container">
-                <div class="text-subtitle-1">
-                    Om du har dina kalkoner i en kommersiell verksamhet måste du ange produktionsplats.
-                </div>
-                <v-text-field label="Produktionsplatsnummer" prefix="SE"
-                    hint="Ange den/de produktionsplatser där du bedrev din produktion 2023. Varje nummer består av upp till 6 siffor.">
-                </v-text-field>
-                <v-text-field v-for="(field, index) in productPlaceFields" :key="index" :label="'Produktionsplatsnummer'"
-                    prefix="SE" v-model="field.value">
-                    <template v-slot:append>
-                        <v-icon @click="removeProductionPlace(index)" :style="{ color: 'red' }">mdi-delete</v-icon>
-                    </template>
-                </v-text-field>
-                <v-btn @click="addProductionPlace" prepend-icon="mdi-plus" color="success" class="my-4">
-                    Lägg till produktionsplats
-                </v-btn>
-            </v-container>
-        </v-form>
-</v-container>
+      <v-container class="productionplace-container">
+        <div class="text-subtitle-1">
+          Om du har dina kalkoner i en kommersiell verksamhet måste du ange
+          produktionsplats.
+        </div>
+        <v-text-field
+          label="Produktionsplatsnummer"
+          prefix="SE"
+          hint="Ange den/de produktionsplatser där du bedrev din produktion 2023. Varje nummer består av upp till 6 siffor."
+        >
+        </v-text-field>
+        <v-text-field
+          v-for="(field, index) in productPlaceFields"
+          :key="index"
+          :label="'Produktionsplatsnummer'"
+          prefix="SE"
+          v-model="field.value"
+        >
+          <template v-slot:append>
+            <v-icon
+              @click="removeProductionPlace(index)"
+              :style="{ color: 'red' }"
+              >mdi-delete</v-icon
+            >
+          </template>
+        </v-text-field>
+        <v-btn
+          @click="addProductionPlace"
+          prepend-icon="mdi-plus"
+          color="success"
+          class="my-4"
+        >
+          Lägg till produktionsplats
+        </v-btn>
+      </v-container>
+    </v-form>
+  </v-container>
 </template>
 
 <script setup>
 const props = defineProps({
   formDataApply: Object,
 });
-</script>
 
+/* defineEmits(["update:formDataApply"]); */
+</script>
 
 <script>
 import cHeader from "./form-components/c-header.vue";
 import cBody from "./form-components/c-body.vue";
 
 export default {
-    components: {
-        cHeader: cHeader,
-        cBody: cBody,
+  components: {
+    cHeader: cHeader,
+    cBody: cBody,
+  },
+  data() {
+    return {
+      productionType: false,
+      showHens: false,
+      field1: null,
+      field2: null,
+      field3: null,
+      showTurkeys: false,
+      field4: null,
+      field5: null,
+      field6: null,
+      productPlaceFields: [],
+    };
+  },
+  computed: {
+    totalHens: function () {
+      return Number(this.field1) + Number(this.field2) + Number(this.field3);
     },
-    data() {
-        return {
-            productionType: false,
-            showHens: false,
-            field1: null,
-            field2: null,
-            field3: null,
-            showTurkeys: false,
-            field4: null,
-            field5: null,
-            field6: null,
-            productPlaceFields: [],
-        }
+    totalTurkeys: function () {
+      return Number(this.field4) + Number(this.field5) + Number(this.field6);
     },
-    computed: {
-        totalHens: function () {
-            return Number(this.field1) + Number(this.field2) + Number(this.field3)
-        },
-        totalTurkeys: function () {
-            return Number(this.field4) + Number(this.field5) + Number(this.field6)
-        }
+  },
+  methods: {
+    addProductionPlace() {
+      this.productPlaceFields.push({ value: "" });
     },
-    methods: {
-        addProductionPlace() {
-            this.productPlaceFields.push({ value: '' });
-        },
-        removeProductionPlace(index) {
-            this.productPlaceFields.splice(index, 1);
-        }
-    }
-}
+    removeProductionPlace(index) {
+      this.productPlaceFields.splice(index, 1);
+    },
+  },
+};
 </script>
 
 <style scoped>
 .productionplace-container {
-    margin-top: 5rem;
+  margin-top: 5rem;
 }
 
 .my-field .v-icon {
-    color: red !important;
+  color: red !important;
 }
 </style>
